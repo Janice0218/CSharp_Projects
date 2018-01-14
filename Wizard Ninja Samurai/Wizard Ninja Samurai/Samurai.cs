52bc62a1c471bd4d96f4c875fed01680a84a7aba
Beth Hart <errordrivendev@gmail.com>
2018-01-13 20:55:59 -0800

    Add inheritance and interface for weapons

---
+++ Wizard Ninja Samurai/Wizard Ninja Samurai/Samurai.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Wizard_Ninja_Samurai
+{
+    public class Samurai : Human
+    {
+        private static int Count = 0;
+        public Samurai(string name, ISecretWeapon weapon) :base(name, weapon)
+        {
+            Health = 200;
+            Count += 1;
+        }
+
+        public static void HowMany() => Console.WriteLine("Samauris: " + Count);
+    }
+}
---
+++ Wizard Ninja Samurai/Wizard Ninja Samurai/Wizard.cs
@@ -0,0 +1,11 @@
+﻿namespace Wizard_Ninja_Samurai
+{
+    public class Wizard : Human
+    {
+        public Wizard(string name, ISecretWeapon weapon) :base(name, weapon)
+        {
+            Health = 50;
+            Intelligence = 25;
+        }
+    }
+}

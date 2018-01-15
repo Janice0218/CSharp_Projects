57c5c8f3b74c3f0a8225a761cbf85347faa71eb9
Beth Hart <errordrivendev@gmail.com>
2018-01-14 19:56:44 -0800

    minor changes -- practicing LINQ

--- DeckOfCards/DeckOfCards/Program.cs
+++ DeckOfCards/DeckOfCards/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DeckOfCards
 {
@@ -9,16 +10,23 @@ namespace DeckOfCards
         static void Main(string[] args)
         {
             var myDeck = new Deck();
-            myDeck.Deal();
-            myDeck.Deal();
+            //myDeck.Deal();
+            //myDeck.Deal();
             myDeck.Shuffle();
-            myDeck.PrintShoe();
-            var player1=new Player("Beth");
+            //myDeck.PrintShoe();
+            var player1 = new Player("Beth");
             player1.Draw(myDeck);
             player1.Draw(myDeck);
-            player1.SeeHand();
-            myDeck.PrintShoe();
-            
+            player1.Draw(myDeck);
+            player1.Draw(myDeck);
+            //player1.SeeHand();
+            //myDeck.PrintShoe();
+            var order = player1.Hand.OrderByDescending(c => c.Face);
+            foreach (var heart in order)
+            {
+                Console.WriteLine(heart);
+            }
+
 
 
         }

94132cb9c2a0465c2af353c1c44c4897c9978725
Beth Hart <errordrivendev@gmail.com>
2018-01-16 20:34:48 -0800

    set up file tree and routing

---
+++ Portfolio/Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("home")]
+        public IActionResult Home()
+        {
+            return View();
+        }
+        [HttpGet("projects")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}

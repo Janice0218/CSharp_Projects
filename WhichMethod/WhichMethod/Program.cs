affd53fd5596d2bfd4ed7580cbe20459b8052337
Beth Hart <errordrivendev@gmail.com>
2018-01-18 18:20:46 -0800

    method override and casting practice

---
+++ WhichMethod/WhichMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WhichMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Worker().WhoAmI();
+            new Farmer().WhoAmI();
+            (new Farmer() as Worker).WhoAmI();
+            var Bob = new Farmer();
+            Worker Wbob = (Worker) Bob;
+            Bob.WhoAmI();
+            Wbob.WhoAmI();
+            new Baker().WhoAmI();
+        }
+    }
+}
---
+++ WhichMethod/WhichMethod/Worker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WhichMethod
+{
+    class Worker
+    {
+        public virtual void WhoAmI()
+        {
+            Console.WriteLine("I am a worker");
+        }
+    }
+
+    class Farmer : Worker
+    {   //override will write over Parent Method for either object
+        //new will just hid Parent Method but can still access when cast
+
+        //public override void WhoAmI()
+        public new void WhoAmI()
+        {
+
+            Console.WriteLine("I am a Farmer");
+        }
+    }
+
+    class Baker : Worker
+    {
+        
+    }
+}

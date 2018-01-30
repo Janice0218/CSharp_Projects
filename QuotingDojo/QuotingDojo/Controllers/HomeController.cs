92ef6c7becba0e3a327b54c290e45d5578a14472
Beth Hart <errordrivendev@gmail.com>
2018-01-29 23:56:39 -0800

    "we have a database!!"

---
+++ QuotingDojo/QuotingDojo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DbConnection;
+using Microsoft.AspNetCore.Mvc;
+using QuotingDojo.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuotingDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+       [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/quotes")]
+        public IActionResult Show()
+        {
+            var query = DbConnector.Query(@"Select * from users");
+            return View(query);
+        }
+
+        [HttpPost("/quotes")]
+        public IActionResult Create(User user)
+        {
+            var query =
+                $@"insert into users(name,quote,created_at, updated_at)values('{user.name}', '{user.quote}', NOW(), NOW())";
+            DbConnector.Execute(query);
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ QuotingDojo/QuotingDojo/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotingDojo.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string quote { get; set; }
+        public string created_at { get; } 
+    }
+}

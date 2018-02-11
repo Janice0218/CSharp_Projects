ec803e6f7272e892745da09aea4f011f6d28359c
Beth Hart <errordrivendev@gmail.com>
2018-02-10 19:17:19 -0800

    edi configuration setting

---
+++ TheWall/TheWall/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DbConnection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TheWall.Models;
+using TheWall.Properties;
+
+namespace TheWall
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+      
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            services.Configure<MySqlOptions>(Configuration.GetSection("ConnectionString"));
+            services.AddScoped<DbConnector>();
+            services.AddMvc();
+        }
+
+  
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
+
